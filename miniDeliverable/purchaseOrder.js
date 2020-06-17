3508367a09ed05b41a0747af62b99b3a7d6c1d31
Seymone Gugneja <seymonegug@gmail.com>
2020-06-16 20:21:05 -0400

    Update purchaseOrder.js

--- miniDeliverable/purchaseOrder.js
+++ miniDeliverable/purchaseOrder.js
@@ -1,3 +1,6 @@
+//Sub total
+
+
 //generates QR Code
 var qrdata = document.getElementById("data");
 var qrcode = new QRCode(document.getElementById("qrcode"));
@@ -6,4 +9,20 @@ function generateQR() {
     var data = qrdata.value;
 
     qrcode.makeCode(data);
+
+    //sub total
+    //doesn't work yet
+    function findTotal(){
+        var arr = document.getElementsByName('subTotal').value;
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+                tot += parseInt(arr[i].value);
+        }
+        document.getElementById('subTot').value = tot;
+    }
+    
+
+
+
 }
